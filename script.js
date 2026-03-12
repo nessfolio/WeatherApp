f35fc3ec91b674bba8528417f236c2257d1da6c7
ness <nessfolio@gmail.com>
2026-03-12 22:59:49 +0600

    refactor: rewrite Promise.then implementation using async/await

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const cordsHTML = document.getElementById('cords');
 const cityHTML = document.getElementById('city');
 const loadingHTML = document.getElementById('loadingCircle');
 
-
+/*
 fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current=temperature_2m,is_day`)
 .then(response => response.json())
 .then(data => {
@@ -27,6 +27,30 @@ fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${l
         },
         (error) => {
                 cityHTML.innerText = ` ${error}`
+                cityHTML.style = 'color: red;'
         }
 )
-.finally(() => loadingHTML.classList.remove('loading-circle'))
+.finally(() => loadingHTML.classList.remove('loading-circle')) */
+
+
+async function showWeather() {
+        try {
+                const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current=temperature_2m,is_day`);
+                const infoWeather = await response.json();
+
+                console.log(infoWeather)
+
+                cityHTML.innerText = ` ${city}`
+                cordsHTML.innerText = `cords: ${latitude}, ${longitude}`
+                isDayHTML.innerText = `${infoWeather.current.is_day == 1 ? 'Day' : 'Night'}`
+                temperatureHTML.innerText = `${infoWeather.current.temperature_2m}°C`
+        } catch(error) {
+                cityHTML.innerText = ` ${error}`
+                cityHTML.style = 'color: red;'
+        } finally {
+                loadingHTML.classList.remove('loading-circle')
+        }
+}
+
+
+showWeather()
