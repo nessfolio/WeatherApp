cb28281667435e6c73f1b41b22465f4470ddbe7b
ness <nessfolio@gmail.com>
2026-03-12 16:22:49 +0600

    feat: show error in UI and handle loading state in finally

--- script.js
+++ script.js
@@ -10,18 +10,23 @@ const cityHTML = document.getElementById('city');
 const loadingHTML = document.getElementById('loadingCircle');
 
 
-fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=is_day,temperature_2m,rain,showers,snowfall,relative_humidity_2m,wind_speed_10m&timezone=${timezone}%2F${city}&timeformat=unixtime`)
+fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m&current=temperature_2m,is_day`)
 .then(response => response.json())
 .then(data => {
         console.log(data)
-        console.log(data.current)
-        console.log(data.current.is_day)
-        console.log(data.current.temperature_2m)
+        // console.log(data.current)
+        // console.log(data.current.is_day)
+        // console.log(data.current.temperature_2m)
 
-        loadingHTML.classList.remove('loading-circle')
+
 
         cityHTML.innerText = ` ${city}`
         cordsHTML.innerText = `cords: ${latitude}, ${longitude}`
         isDayHTML.innerText = `${data.current.is_day == 1 ? 'Day' : 'Night'}`
         temperatureHTML.innerText = `${data.current.temperature_2m}°C`
-})
+        },
+        (error) => {
+                cityHTML.innerText = ` ${error}`
+        }
+)
+.finally(() => loadingHTML.classList.remove('loading-circle'))
